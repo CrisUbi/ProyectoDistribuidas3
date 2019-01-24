7f1a2fba5bb3cd6ba75a5acce8dcafc17c019415
José Arellano <32889739+jlpepelucho@users.noreply.github.com>
2019-01-24 11:46:46 -0500

    Modificacion Ingreso Actividades

--- prj_RMI_interface_jlal/build/generated-sources/ap-source-output/pkg_interface/LibroCuda_.java
+++ prj_RMI_interface_jlal/build/generated-sources/ap-source-output/pkg_interface/LibroCuda_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-24T11:10:44")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2019-01-24T11:32:19")
 @StaticMetamodel(LibroCuda.class)
 public class LibroCuda_ { 
 
